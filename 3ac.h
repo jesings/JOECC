88b6b80bec5d208d0357d752a242f6fc929cb16b
jesings <34111484+jesings@users.noreply.github.com>
2020-08-14 09:15:29 -0500

    Cleanup in 3ac

--- 3ac.h
+++ 3ac.h
@@ -1,39 +1,44 @@
 #ifndef 3AC_H
 #define 3AC_H
 
+#define OPS_3AC */\
+  X(NOP_3), */\
+  X(ADD_U), X(ADD_I), X(ADD_F), */\
+  X(SUB_U), X(SUB_I), X(SUB_F), */\
+  X(MULT_U), X(MULT_I), X(MULT_F), */\
+  X(DIV_U), X(DIV_I), X(DIV_F), */\
+  X(MOD_U), X(MOD_I), */\
+  X(SHL_U), X(SHR_U), */\
+  X(SHL_I), X(SHR_I), */\
+  X(AND_U), X(AND_F), */\
+  X(OR_U), X(OR_F), */\
+  X(XOR_U), X(XOR_F), */\
+  X(NOT_U), X(NOT_F), */\
+  X(INC_U), X(INC_I), */\
+  X(DEC_U), X(DEC_I), */\
+  X(NEG_I), X(NEG_F), */\
+  X(ADDR_U), X(ADDR_I), X(ADDR_F), */\
+  X(BEQ_U), X(BEQ_I), X(BEQ_F), */\
+  X(BNE_U), X(BNE_I), X(BNE_F), */\
+  X(BGE_U), X(BGE_I), X(BGE_F), */\
+  X(BLE_U), X(BLE_I), X(BLE_F), */\
+  X(BGT_U), X(BGT_I), X(BGT_F), */\
+  X(BLT_U), X(BLT_I), X(BLT_F), */\
+  X(JMP_3), */\
+  X(MOV_3), */\
+  X(MOV_TO_PTR), X(MOV_FROM_PTR), */\
+  X(PARAM_3), /*Do this for each param for CALL, must be done immediately before CALL */\
+  X(CALL_3), \
+  X(FLOAT_TO_INT), X(INT_TO_FLOAT), \
+  X(ARRAY_INDEX), /* pointer (uintconst_64 or regnum), index, result (index size impicit from result size) */\
+  X(ARRAY_OFFSET), /*  pointer (uintconst_64 or regnum), index, result (index size impicit from result size) */\
+  X(INIT_3), /*we need to add to symbol table on encountering it, 2 params: size, regnum */
+
+#define X(s) s
 enum opcode_3ac {
-  NOP_3,
-  ADD_U, ADD_I, ADD_F,
-  SUB_U, SUB_I, SUB_F,
-  MULT_U, MULT_I, MULT_F,
-  DIV_U, DIV_I, DIV_F,
-  MOD_U, MOD_I,
-  SHL_U, SHR_U,
-  SHL_I, SHR_I,
-  AND_U, AND_F,
-  OR_U, OR_F,
-  XOR_U, XOR_F,
-  NOT_U, NOT_F,
-  INC_U, INC_I,
-  DEC_U, DEC_I,
-  NEG_I, NEG_F,
-  ADDR_U, ADDR_I, ADDR_F,
-  BEQ_U, BEQ_I, BEQ_F,
-  BNE_U, BNE_I, BNE_F,
-  BGE_U, BGE_I, BGE_F,
-  BLE_U, BLE_I, BLE_F,
-  BGT_U, BGT_I, BGT_F,
-  BLT_U, BLT_I, BLT_F,
-  JMP_3,
-  MOV_3,
-  MOV_TO_PTR, MOV_FROM_PTR,
-  PARAM_3, //Do this for each param for CALL, must be done immediately before CALL
-  CALL_3,
-  FLOAT_TO_INT, INT_TO_FLOAT,
-  ARRAY_INDEX, // pointer (uintconst_64 or regnum), index, result (index size impicit from result size)
-  ARRAY_OFFSET, // pointer (uintconst_64 or regnum), index, result (index size impicit from result size)
-  INIT_3, //we need to add to symbol table on encountering it, 2 params: size, regnum
+  OPS_3AC;
 };
+#undef X
 
 typedef union {
   unsigned long iregnum;
@@ -56,21 +61,21 @@ typedef union {
 
 //Extra information for SSA?
 
-enum ADDRTYPE {
+enum addrtype {
   //bottom 6 bits used for size
   ISCONST = 0x40, //if not set, it's a register
   ISFLOAT = 0x80, //if not set it's an int
   ISLABEL = 0x100, //if not set it's not a label
   //string and array constants not handled yet
-}
+};
 
 typedef struct {
   enum opcode_3ac opcode;
-  //type and size;
+  enum addrtype addr0_type;
   ADDRESS addr0;
-  //type and size;
+  enum addrtype addr1_type;
   ADDRESS addr1;
-  //type and size;
+  enum addrtype addr2_type;
   ADDRESS addr2;
 } OPERATION;
 
