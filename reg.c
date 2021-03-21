49c83921f810d1968d2f7f52ca3318b55e406639
jesings <34111484+jesings@users.noreply.github.com>
2021-03-20 21:12:18 -0500

    Working on information for codegen

--- reg.c
+++ reg.c
@@ -1,6 +1,25 @@
 #include "3ac.h"
 
-char* ireg[] = {"ax", "bx", "cx", "dx", "di", "si", "bp", "sp", "8", "9", "10", "11", "12", "13", "14", "15"};
+char* ireg64[] = {"rax", "rbx", "rcx", "rdx", "rdi", "rsi", "rbp", "rsp", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
+char* ireg32[] = {"eax", "ebx", "ecx", "edx", "edi", "esi", "ebp", "esp", "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d"};
+char* ireg16[] = {"ax", "bx", "cx", "dx", "di", "si", "bp", "sp", "r8w", "r9w", "r10w", "r11w", "r12w", "r13w", "r14w", "r15w"};
+char* ireg8[] = {"al", "bl", "cl", "dl", "dil", "sil", "bpl", "spl", "r8b", "r9b", "r10b", "r11b", "r12b", "r13b", "r14b", "r15b"};
+char* freg128[] = {"xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7", "xmm8", "xmm9", "xmm10", "xmm11", "xmm12", "xmm13", "xmm14", "xmm15"};
+char* freg256[] = {"ymm0", "ymm1", "ymm2", "ymm3", "ymm4", "ymm5", "ymm6", "ymm7", "ymm8", "ymm9", "ymm10", "ymm11", "ymm12", "ymm13", "ymm14", "ymm15"};
+
+struct opinfo {
+  char* opname;
+  int numargs;
+  //clobbers?
+  //anything else?
+};
+
+struct opinfo op2op[] = {
+  [NOP_3] = {"nop", 0},
+  [LBL_3] = {"", 1}, //not sure?
+  [RET_0] = {"ret", 0}, //not sure?
+  [ADD_U] = {"add", 2}, //not sure?
+};
 //fregs are just xmm%d, we'll do 0-15
 //handle long doubles same as doubles
 //
