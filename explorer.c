20e2a8e0171e0e6c533cf89006e9f8b783521d2e
jesings <34111484+jesings@users.noreply.github.com>
2022-01-01 21:22:02 -0500

    Terminal cooking

--- explorer.c
+++ explorer.c
@@ -8,6 +8,23 @@
 #define CLEAR "\033[H\033[J"
 
 void display_block(PROGRAM* prog, BBLOCK* blk) {
-  puts(CLEAR);
+  //assert stdin/stdout both terminals??
+  struct termios orig_attrs;
+  struct termios new_attrs;
+  tcgetattr(STDIN_FILENO, &orig_attrs);
+  new_attrs = orig_attrs;
+  new_attrs.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | IEXTEN; //stop cooking the output!
+  new_attrs.c_cc[VMIN] = 1; //reads go character by character
+  new_attrs.c_cc[VTIME] = 0; //never timeout on the read
 
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_attrs);
+  char readchar;
+  while(read(STDIN_FILENO, &readchar, 1) > 0) {
+    puts(CLEAR);
+    switch(readchar) {
+    }
+  }
+
+cleanup:
+  tcsetattr(STDIN_FILENO, TCSANOW, &orig_attrs);
 }
